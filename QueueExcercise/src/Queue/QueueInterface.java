a297badc6ad8a1577e3cad5400b0236299544446
witoldzol <blue3656@gmail.com>
2018-02-03 15:42:06 +0000

    's'

---
+++ QueueExcercise/src/Queue/QueueInterface.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Queue;
+
+/**
+ *
+ * @author x17143853
+ */
+public interface QueueInterface {
+    
+    public void enqueue(Object obj );
+    
+    public void dequeue();
+    
+    public int size();
+    
+    public boolean isEmpty();
+    
+    public Object front();
+    
+    
+    
+}
